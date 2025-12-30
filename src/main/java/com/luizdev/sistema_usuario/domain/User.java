126049f6372f0b95fb76ea04aa04004c1eabfa4c
DevLuiz-eng <luiznotbook2@gmail.com>
2025-12-30 10:40:49 -0300

    feat: added lombok annotations

--- src/main/java/com/luizdev/sistema_usuario/domain/User.java
+++ src/main/java/com/luizdev/sistema_usuario/domain/User.java
@@ -7,8 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
 

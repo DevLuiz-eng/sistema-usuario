d2ae48feacb5bdccb50f34749d603d8dff01d6c0
DevLuiz-eng <luiznotbook2@gmail.com>
2025-12-30 10:10:00 -0300

    Initial commit: project initialization

---
+++ src/test/java/com/luizdev/sistema_usuario/SistemaUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizdev.sistema_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

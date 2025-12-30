5cd6e25caee39f0e1ffe1c5affd9dc6736f0d0b8
DevLuiz-eng <luiznotbook2@gmail.com>
2025-12-30 10:48:39 -0300

    refactor: added new validation

--- src/main/java/com/luizdev/sistema_usuario/dto/UserRequest.java
+++ src/main/java/com/luizdev/sistema_usuario/dto/UserRequest.java
@@ -1,7 +1,8 @@
 package com.luizdev.sistema_usuario.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
-public record UserRequest(@NotNull String name, @NotNull String email, @Min(1) @NotNull int age) {
+public record UserRequest(@NotNull String name, @NotNull @Email String email, @Min(1) @NotNull int age) {
 }
